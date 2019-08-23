94ba8dbb835ae4cdf9e1372eb2d25fe19115010d
Ryan Morgan <rrgy80@gmail.com>
2019-08-23 08:36:41 -0600

    all that I did

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -2,81 +2,109 @@
 
 // Create a variable called 'myName' that's value is your name
 
-// Code here
+const myName = 'Ryan'
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' that's value is your favorite number
 
-//Code here
+const faveNum = 5
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and set it to true
 
-//Code here
+const lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, num1 and num2, and returns the sum of those two parameters.
 
-//Code here
+function sum (num1, num2){
+	return num1 + num2
+}
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. If it is, return true as a boolean. If x does not equal true, return false as a boolean
 
-//Code here
+function lovesCodeChecker(x){
+	if (x === true){
+		return true
+	} else {return false}
+}
 
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
 
-//Code here
+function oddChecker (num){
+	if (num% 2 !== 0){
+		return 'the number is odd'
+	} else {return 'the number is even'}
+}
 
 //////////////////PROBLEM 7////////////////////
 
 // Create a function called 'iLove' that takes in two parameters, name which will be a string  and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
 
-//Code Here
+function iLove (name, love){
+	return function(){
+		return name+'loves'+ love 
+	}
+}
 
 //////////////////PROBLEM 8////////////////////
 
 // Create a copy of the faveColors array called 'colorCopy' using slice
 const faveColors = ['red', 'green', 'black']
 
-//Code Here
+const colorCopy = faveColors.slice(0)
 
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push
 
-//Code Here
+colorCopy.push (1)
 
 //////////////////PROBLEM 10////////////////////
 
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
 const numbers = [1, 2, 3, 4, 5]
 
-//Code Here
+const middleNums = numbers.slice (1,4)
 
 //////////////////PROBLEM 11////////////////////
 
 // Create an object called 'me' that has these keys with the correct values: firstName, state, age, greeter. firstName will be your name as a string. state will be your current state or providence as a string. age will be your age as a number. greeter will be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the correct values
 
-//Code Here
+const me = {
+	firstName: 'Ryan',
+	state: 'UT',
+	age: 27,
+	greeter:""
+}
 
 //////////////////PROBLEM 12////////////////////
 
 // Create a function called 'bigOrSmall' that takes in one parameter, arr. Create a new array inside of bigOrSmall called 'answers'. Loop over the passed in arr param. If the number is GREATER than 100, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function
 
-// Code here
+function bigOrSmall(arr){
+	for (i = 0; i < arr.length; i++){
+		if (i > 100){
+			return 'big'
+		} else if(i <= 100){
+			return 'small'
+		}
+	}
+	let answers = arr[i]
+}
 
 //////////////////PROBLEM 13////////////////////
 
 // Create a function called 'arrayReverser' that takes in one parameter, arr. Inside of arrayReverser, create an empty array called 'reversed'. Using a for loop, loop over the passed in array in reverse and add each item to the new reversed array. Finally, return the new reversed array
 
-//Code Here
+
 
 //////////////////PROBLEM 14 - 18 Setup////////////////////
 
@@ -88,7 +116,7 @@ const myNumbers = [3, 55, 788, 2, 1]
 
 // First, let's use .map to create a new array that doubles each number. Call the new array 'doubled'
 
-// Code Here
+
 
 //////////////////PROBLEM 15////////////////////
 
--- javascript-2/student.js
+++ javascript-2/student.js
@@ -4,27 +4,29 @@
 //////////////////STEP 1////////////////////
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
 
-// Code Here
+const lovesCode = true
 
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
-
-// Code Here
+const age = 27
 
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
 
-// Code Here
+const name = 'Ryan'
 
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
 
-// Code Here
+const phrase = ''
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
-// Code Here
-
+function step5(){
+    if (lovesCode === true){
+    lovesCode ='I love code'
+    } else {lovesCode = 'I do not love code'}
+}
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
